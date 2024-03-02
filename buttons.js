f746c3384c345034145b40b65956f2b6641b89a8
Eric Zhang <ezhang588@gmail.com>
2024-03-02 16:39:51 -0600

    Updated website, added animations, flow, started in light/dark mode

---
+++ buttons.js
@@ -0,0 +1,16 @@
+window.scrollTo(0, 0);
+
+function changeTheme(event) {
+  const button = event.target;
+	const myImg = document.getElementById("myButton");
+	if (myImg.src.match("images/moon.png")) {
+		myImg.src = "images/sun.png";
+
+		
+	} else {
+		myImg.src = "images/moon.png";
+
+		document.getElementById
+
+	}
+}
