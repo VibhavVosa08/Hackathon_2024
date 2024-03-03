ce8164a3cf7aff164a4023867b18404f7e8be6e1
VibhavVosa08 <114874342+VibhavVosa08@users.noreply.github.com>
2024-03-02 22:20:04 -0600

    Almost Final

--- buttons.js
+++ buttons.js
@@ -1,16 +1,36 @@
-window.scrollTo(0, 0);
+let lightMode = localStorage.getItem("lightMode");
+let darkMode = localStorage.getItem("darkMode");
+
+const enableLightMode = () => {
+	document.body.classList.add('lightmode');
+	localStorage.setItem('lightMode', 'enabled');
+	document.body.classList.remove('darkmode');
+	localStorage.setItem("darkMode", null);
+}
+
+const disableLightMode = () => {
+	document.body.classList.remove('lightmode');
+	localStorage.setItem("lightMode", null);
+	document.body.classList.add('darkmode');
+	localStorage.setItem("darkMode", 'enabled');
+}
 
 function changeTheme(event) {
-  const button = event.target;
+	const button = event.target;
+	lightMode = localStorage.getItem("lightMode");
+	darkMode = localStorage.getItem("darkMode");
 	const myImg = document.getElementById("myButton");
-	if (myImg.src.match("images/moon.png")) {
+	if (lightMode == "enabled") {
 		myImg.src = "images/sun.png";
+		disableLightMode();
+		document.body.style.backgroundColor = "#ecf2fe";
+    document.body.style.h1 = "#232D3F";
 
-		
 	} else {
 		myImg.src = "images/moon.png";
-
-		document.getElementById
+		enableLightMode();
+		document.body.style.backgroundColor = "#232D3F";
+    document.body.style.h1 = "#232D3F";
 
 	}
 }
